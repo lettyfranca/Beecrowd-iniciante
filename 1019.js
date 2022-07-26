bc4dfbf693c1c6ef8ff34f8d9ce8c723d2947773
lettyfranca <leeffrance@gmail.com>
2022-07-26 20:36:10 -0300

    Desafio 1019 - Conversão de Tempo

--- 1019.js
+++ 1019.js
@@ -3,4 +3,20 @@
  * Leia um valor inteiro, que é o tempo de duração em segundos de um determinado evento em uma fábrica, e informe-o expresso no formato horas:minutos:segundos.
  */
 let input = require('fs').readFileSync('stdin', 'utf-8');
-let segundos = input.split('\n');
+
+//entrada dos dados
+let segundos = parseInt(input);
+
+//processamento dos dados
+let horas, minutos, restoHoras, restoMinutos, restoSegundos;
+
+horas = parseInt(segundos / 3600);
+restoHoras = segundos % 3600;
+
+minutos = parseInt(restoHoras / 60);
+restoMinutos = restoHoras % 60;
+
+segundos = restoMinutos;
+
+//saída dos dados
+console.log(`${horas}:${minutos}:${segundos}`);
