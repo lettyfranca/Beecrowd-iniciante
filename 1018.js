e6b053e7da5dfd260377500ecb40b50e5b2bd82c
lettyfranca <leeffrance@gmail.com>
2022-07-25 22:13:26 -0300

    Início - desafio 1018

---
+++ 1018.js
@@ -0,0 +1,7 @@
+/**
+ * @author Letícia França
+ * Leia um valor inteiro. A seguir, calcule o menor número de notas possíveis (cédulas) no qual o valor pode ser decomposto. As notas consideradas são de 100, 50, 20, 10, 5, 2 e 1. A seguir mostre o valor lido e a relação de notas necessárias.
+ */
+let input = require('fs').readFileSync('stdin', 'utf-8');
+let valor = input.split('\n');
+
